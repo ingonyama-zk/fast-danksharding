69f408841f7db754ce88759c3ba3ae88da9d4749
vhnatyk <vhnatyk@gmail.com>
2023-03-16 16:24:01 +0100

    fix building for updated flow

---
+++ fast-danksharding/build.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+fn main() {
+    //TODO: check cargo features selected
+    //TODO: can conflict/duplicate with make ?
+
+    println!("cargo:rerun-if-env-changed=CXXFLAGS");
+    println!("cargo:rerun-if-changed=../icicle");
+
+    let arch_type = env::var("ARCH_TYPE")
+        .unwrap_or(String::from("native"));
+
+    let mut arch = String::from("-arch=");
+    arch.push_str(&arch_type);
+
+    let mut nvcc = cc::Build::new();
+
+    println!("Compiling icicle library using arch: {}", &arch);
+
+    nvcc.cuda(true);
+    nvcc.debug(false);
+    nvcc.flag(&arch);
+    nvcc.files([
+        "./src/cuda/lib.cu",
+    ]);
+    nvcc.compile("ingo_dnkshard"); //TODO: extension??
+}
