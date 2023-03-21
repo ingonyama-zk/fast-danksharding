efcc5640400274315a6847d8f5a33bed159423cc
Vitalii Hnatyk <vhnatyk@gmail.com>
2023-03-21 20:05:20 +0200

    update to flow refernce implementation (#15)

--- fast-danksharding/src/matrix.rs
+++ fast-danksharding/src/matrix.rs
@@ -61,6 +61,42 @@ pub fn interleave_cols<T: Copy>(a: &Vec<Vec<T>>, b: &Vec<Vec<T>>) -> Vec<Vec<T>>
     result
 }
 
+pub fn rows_to_cols_flatten<T: Copy>(rows_flat: &[T], row_len: usize) -> Vec<T> {
+    let rows = rows_flat.len() / row_len;
+    let mut cols_flat = Vec::with_capacity(rows_flat.len());
+
+    for c in 0..row_len {
+        for r in 0..rows {
+            let index = r * row_len + c;
+            cols_flat.push(rows_flat[index]);
+        }
+    }
+
+    cols_flat
+}
+
+pub fn rows_to_cols_inplace<T: Copy>(rows_flat: &mut [T], row_len: usize) {
+    let rows = rows_flat.len() / row_len;
+    let mut index = 1;
+    let mut next;
+
+    for c in 0..row_len {
+        for r in 1..rows {
+            let current = r * row_len + c;
+            next = current;
+
+            while next >= index {
+                let temp = rows_flat[index - 1];
+                rows_flat[index - 1] = rows_flat[next];
+                rows_flat[next] = temp;
+
+                next = next - row_len;
+            }
+            index += 1;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,16 +111,8 @@ mod tests {
             vec![21, 22, 23, 24, 25],
         ];
 
-        let all_rows: &[Vec<u32>] = &matrix[..];
         let all_cols = rows_to_cols(&matrix);
 
-        let expected_rows: &[Vec<u32>] = &[
-            vec![1, 2, 3, 4, 5],
-            vec![6, 7, 8, 9, 10],
-            vec![11, 12, 13, 14, 15],
-            vec![16, 17, 18, 19, 20],
-            vec![21, 22, 23, 24, 25],
-        ];
         let expected_cols: &[Vec<u32>] = &[
             vec![1, 6, 11, 16, 21],
             vec![2, 7, 12, 17, 22],
@@ -93,9 +121,34 @@ mod tests {
             vec![5, 10, 15, 20, 25],
         ];
 
-        assert_eq!(all_rows, expected_rows);
         assert_eq!(all_cols, expected_cols);
-        assert_eq!(expected_cols[4][2], matrix[2][4])
+        assert_eq!(expected_cols[4][2], matrix[2][4]);
+    }
+
+    #[test]
+    fn test_rows_to_cols_flat() {
+        let rows_flat = vec![
+            1, 2, 3, 4, 5, //
+            6, 7, 8, 9, 10, //
+            11, 12, 13, 14, 15, //
+            16, 17, 18, 19, 20, //
+            21, 22, 23, 24, 25, //
+        ];
+
+        let all_cols = rows_to_cols_flatten(&rows_flat, 5);
+
+        let expected_cols_flat = vec![
+            1, 6, 11, 16, 21, //
+            2, 7, 12, 17, 22, //
+            3, 8, 13, 18, 23, //
+            4, 9, 14, 19, 24, //
+            5, 10, 15, 20, 25, //
+        ];
+
+        assert_eq!(all_cols, expected_cols_flat);
+
+        let all_cols = rows_to_cols_flatten(&all_cols, 5);
+        assert_eq!(all_cols, rows_flat);
     }
 
     #[test]
