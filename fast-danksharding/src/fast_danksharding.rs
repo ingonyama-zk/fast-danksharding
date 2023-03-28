6cdcbbca9c4a35faf23ecd0ee31931d7f72a815e
vhnatyk <vhnatyk@gmail.com>
2023-03-28 16:15:03 +0200

    assert first branch

--- fast-danksharding/src/fast_danksharding.rs
+++ fast-danksharding/src/fast_danksharding.rs
@@ -74,9 +74,9 @@ pub fn main_flow() {
     // K = [K0, K1]  // 2*N_POINTS x 1 (512x1 commitments)
     let K = [K0, K1].concat();
     println!("K {:0.3?}", br1_time.elapsed());
-    debug_assert_eq!(K, get_debug_data_points_proj_xy1_vec("K.csv", 2 * N_ROWS));
-
+    
     println!("Branch1 {:0.3?}", br1_time.elapsed());
+    assert_eq!(K, get_debug_data_points_proj_xy1_vec("K.csv", 2 * N_ROWS));
 
     ////////////////////////////////
     println!("Branch 2");
